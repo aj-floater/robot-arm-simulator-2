959338fb06fcc8298eea6fd1d4fa9f10ebfb9292
Archie James <arjames1618@gmail.com>
2023-02-16 10:49:55 +0000

    first

--- modules/window-manager.js
+++ modules/window-manager.js
@@ -1,17 +1,4 @@
-let border = button_constant * 0.25;
-let window_space_left = button_constant + border;
-let window_space_top = top_height + border;
-
-let isRecording = false;
-
-var side = "";
-let left = null;
-let right = null;
-let current_window = null;
-
-// FUNCTIONS ========================================================================================
-
-// Function to change the canvas size while keeping the aspect ratio
+// Function to change the canvas size
 function changeWindowSize(window, width, height) {
   window.canvas.width = width;
   window.canvas.height = height;
@@ -21,65 +8,91 @@ function changeWindowSize(window, width, height) {
   window.camera.updateProjectionMatrix();
 }
 
-// function to move a window
 function moveWindow(window, left, top) {
   window.canvas.style.left = left + "px";
   window.canvas.style.top = top + "px";
 }
 
+let border = button_constant * 0.25;
+let window_space_left = button_constant + border;
+let window_space_top = top_height + border;
+
+let isRecording = false;
+
+let windows = [viewport, collisions, biem];
+let current_window = null;
+var side = "";
+
 let mouseMoveHandler = function(event){};
-function addMouseDownEventListener(button, window){
-  button.addEventListener("mousedown", function() {
-    isRecording = true;
 
-    button.style.cursor = "context-menu"
-    mouseMoveHandler = function(event) {
-      handleMouseMovement(event, window);
-    };
-    document.addEventListener("mousemove", mouseMoveHandler)
-  });
-}
+changeWindowSize(viewport, ws_width - border*2, ws_height - border*2);
+moveWindow(viewport, window_space_left, window_space_top);
+let viewport_btn = document.getElementById("viewport_btn"); // Get the viewport button element
+viewport_btn.addEventListener("click", function() { // Add a click event listener to the viewport button
+  if (viewport.shown())
+    viewport.hide()
+  else {
+    viewport.show(); // Show the viewport scene
+    // Hide the collisions and biem scenes
+    collisions.hide();
+    biem.hide();
+  }
+});
+viewport_btn.addEventListener("mousedown", function() {
+  isRecording = true;
 
-function handleMouseMovement(event, window){
-  current_window = window;
-  document.body.style.cursor = "context-menu";
-  window.canvas.style.zIndex=1;
-  window.show();
-  if (event.clientX < button_constant + ws_width/2){
-    side = "left";
-    if (left==null){
-      moveWindow(window, button_constant+border, top_height+border);
-      changeWindowSize(window, ws_width/2-border*1.5, ws_height-border*2);
-    }
-    else{
-      var rect = left.canvas.getBoundingClientRect();
-      moveWindow(window, rect.left, rect.top);
-      changeWindowSize(window, left.canvas.width, left.canvas.height);
-      if (right == window) {
-        right = null;
-      }
-    }
+  viewport_btn.style.cursor = "context-menu"
+  mouseMoveHandler = function(event) {
+    handleMouseMovement(event, viewport);
+  };
+  document.addEventListener("mousemove", mouseMoveHandler)
+});
+
+changeWindowSize(collisions, ws_width - button_constant * 0.5, ws_height - button_constant * 0.5);
+moveWindow(collisions, window_space_left, window_space_top);
+let collisions_btn = document.getElementById("collisions_btn"); // Get the collisions button element
+collisions_btn.addEventListener("click", function() { // Add a click event listener to the viewport button
+  if (collisions.shown())
+    collisions.hide()
+  else {
+    collisions.show(); // Show the viewport scene
+    // Hide the viewport and biem scenes
+    viewport.hide();
+    biem.hide();
   }
-  if (event.clientX > button_constant + ws_width/2){
-    side = "right";
-    if (right==null){
-      moveWindow(window, button_constant+ws_width/2+border*0.5, top_height+border);
-      changeWindowSize(window, ws_width/2-border*1.5, ws_height-border*2);
-      if (left != null){
-        moveWindow(left, button_constant+border, top_height+border);
-        changeWindowSize(left, ws_width/2-border*1.5, ws_height-border*2);
-      }
-    }
-    else{
-      var rect = right.canvas.getBoundingClientRect();
-      moveWindow(window, rect.left, rect.top);
-      changeWindowSize(window, right.canvas.width, right.canvas.height);
-      if (left == window) {
-        left = null;
-      }
-    }
+});
+collisions_btn.addEventListener("mousedown", function() {
+  isRecording = true;
+
+  collisions_btn.style.cursor = "context-menu"
+  mouseMoveHandler = function(event) {
+    handleMouseMovement(event, collisions);
+  };
+  document.addEventListener("mousemove", mouseMoveHandler)
+});
+
+changeWindowSize(biem, ws_width - button_constant * 0.5, ws_height - button_constant * 0.5);
+moveWindow(biem, window_space_left, window_space_top);
+let biem_btn = document.getElementById("biem_btn"); // Get the biem button element
+biem_btn.addEventListener("click", function() { // Add a click event listener to the biem button
+  if (biem.shown())
+    biem.hide()
+  else {
+    biem.show(); // Show the viewport scene
+    // Hide the viewport and collisions scenes
+    viewport.hide();
+    collisions.hide();
   }
-}
+});
+biem_btn.addEventListener("mousedown", function() {
+  isRecording = true;
+
+  biem_btn.style.cursor = "context-menu"
+  mouseMoveHandler = function(event) {
+    handleMouseMovement(event, biem);
+  };
+  document.addEventListener("mousemove", mouseMoveHandler)
+});
 
 document.addEventListener("mouseup", function(){
   if (isRecording){
@@ -90,51 +103,50 @@ document.addEventListener("mouseup", function(){
     biem_btn.style.cursor = "default"
 
     current_window.canvas.style.zIndex=0;
-
     if (side=="left"){
-      if (left != current_window) {
-        if (left != null) 
-          left.hide();
-        left=current_window;
-
-        if (current_window==right)
-          right = null;
-      }
+      if (viewport.shown()&&viewport.side=="left") {viewport.side=""; viewport.hide;}
+      if (collisions.shown()&&collisions.side=="left") {collisions.side=""; collisions.hide;}
+      if (biem.shown()&&biem.side=="left") {biem.side=""; biem.hide;}
     }
     if (side=="right"){
-      if (right != current_window) {
-        if (right != null) 
-          right.hide();
-        right=current_window;
-
-        if (current_window==left)
-          left = null;
-      }
-    }
-    if (left != null && right == null){
-      changeWindowSize(left, ws_width - button_constant * 0.5, ws_height - button_constant * 0.5);
-      moveWindow(left, window_space_left, window_space_top);
-    }
-    if (right != null && left == null){
-      changeWindowSize(right, ws_width - button_constant * 0.5, ws_height - button_constant * 0.5);
-      moveWindow(right, window_space_left, window_space_top);
-      left = right;
-      right = null;
+      if (viewport.shown()&&viewport.side=="right") {viewport.side=""; viewport.hide;}
+      if (collisions.shown()&&collisions.side=="right") {collisions.side=""; collisions.hide;}
+      if (biem.shown()&&biem.side=="right") {biem.side=""; biem.hide;}
     }
 
-    side="";
-    current_window = null;
-    isRecording=false;
+    current_window.side=side; 
   }
 });
 
-// MAIN ============================================================================================
+function findWindow(){
+  windows.forEach(function(element) {
+    if (element.shown()) return element;
+  });
+}
 
-let viewport_btn = document.getElementById("viewport_btn"); // Get the viewport button element
-addMouseDownEventListener(viewport_btn, viewport);
+function handleMouseMovement(event, window){
+  document.body.style.cursor = "context-menu";
+  window.canvas.style.zIndex=1;
+  if (event.clientX < button_constant + ws_width/2){
+    side = "left";
+    current_window = findWindow();
+    window.show();
+    
+    moveWindow(current_window, button_constant+ws_width/2+border*0.5, top_height+border);
+    changeWindowSize(current_window, ws_width/2-border*1.5, ws_height-border*2);
+    
+    moveWindow(window, button_constant+border, top_height+border);
+    changeWindowSize(window, ws_width/2-border*1.5, ws_height-border*2);
+  }
+  else if (event.clientX > button_constant + ws_width/2){
+    side = "right";
+    current_window = findWindow();
+    window.show();
 
-let collisions_btn = document.getElementById("collisions_btn"); // Get the collisions button element
-addMouseDownEventListener(collisions_btn, collisions);
-
-let biem_btn = document.getElementById("biem_btn"); // Get the biem button element
-addMouseDownEventListener(biem_btn, biem);
+    moveWindow(current_window, button_constant+border, top_height+border);
+    changeWindowSize(current_window, ws_width/2-border*1.5, ws_height-border*2);
+    
+    moveWindow(window, button_constant+ws_width/2+border*0.5, top_height+border);
+    changeWindowSize(window, ws_width/2-border*1.5, ws_height-border*2);
+  }
+}
